97926f70f9983832a46c735d4a6cf7a87ba1d01b
Unknown <bo0842372@otc.edu>
2017-11-06 13:13:29 -0600

    Started use of API
    Uses Vue and Vue-Response as well as MathFacts API

---
+++ main.js
@@ -0,0 +1,14 @@
+$(function () {
+    var vue = new vue({
+        el: '#app',
+        data: {
+            text: null,
+            number: null
+        },
+        methods: {
+            getFact() {
+                
+            }
+        }
+    })
+})
